249860fd3936eb63d81861eeede8309f2c962b18
Md Jamil Akter <jamilakterup@gmail.com>
2023-06-14 10:37:20 +0600

    decodec email ok

--- index.js
+++ index.js
@@ -179,29 +179,6 @@ async function run() {
         });
 
 
-        // -----**************
-        // if the upper code is running than bottom code will removed ------------
-        // -----**************
-
-
-        // app.get('/carts', async (req, res) => {
-        //     const email = req.query.email;
-
-        //     if (!email) {
-        //         res.send([]);
-        //     }
-
-        //     // const decodedEmail = req.decoded.email;
-        //     // if (email !== decodedEmail) {
-        //     //     return res.status(403).send({error: true, message: 'forbidden access'})
-        //     // }
-
-        //     const query = {email: email};
-        //     const result = await cartCollection.find(query).toArray();
-        //     res.send(result);
-        // });
-
-
 
         app.delete('/carts/:id', async (req, res) => {
             const id = req.params.id;
